ba5b4e04c07363568795db83fc2c22e70cd5a900
Lucas Gabriel F <lucasg.fregolent.proj@gmail.com>
2024-10-05 20:47:59 -0300

    ok

---
+++ src/components/atoms/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../../styles/Card.css"
+
+const Card: React.FC = () => {
+    return (
+        <div className='card-container'>
+            <p>fjh kjdhskjhj</p>
+        </div>
+    );
+};
+
+export default Card;
--- src/components/pages/ShuttleTravel.tsx
+++ src/components/pages/ShuttleTravel.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import "../../styles/ShuttleTravel.css"
 import europaFlipper from "../../assets/images/Europa_flipper.gif";
+import Card from "../atoms/Card"
 
 
 const ShuttleTravel = () => {
   return (
     <div className="space-container">
       <img src={europaFlipper} alt="Spaceship" className="spaceship" />
+      <div className='informative-elements'>
+        <Card />
+      </div>
     </div>
   );
 };
