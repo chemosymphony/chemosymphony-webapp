937f3759e5d497da2dbc3af70a5e17018c7adf08
Gustavo Gitzel <gustavo.gitzel@ifood.com.br>
2024-10-06 23:13:57 -0300

    fix audio

--- src/components/atoms/BackgroundMusic.tsx
+++ src/components/atoms/BackgroundMusic.tsx
@@ -29,7 +29,7 @@ const BackgroundMusic = forwardRef((_, ref) => {
     const audioRef = useRef<HTMLAudioElement>(null);
     const [isPlaying, setIsPlaying] = useState(localStorage.getItem('isPlaying') === 'true');
     const [isMuted, setIsMuted] = useState(localStorage.getItem('isMuted') === 'true');
-    const [hasError, setHasError] = useState(false)
+    const [hasError, setHasError] = useState(localStorage.getItem('hasError') === 'true');
 
     useImperativeHandle(ref, () => ({
         play: () => {
@@ -46,11 +46,13 @@ const BackgroundMusic = forwardRef((_, ref) => {
                 if (audioRef.current && localStorage.getItem('isPlaying') === 'true') {
                     await audioRef.current.play();
                     setIsPlaying(true);
-                    setHasError(false)
+                    setHasError(false); // Sem erro, reseta o estado
+                    localStorage.setItem('hasError', 'false'); // Armazena o estado de erro
                 }
             } catch (err) {
                 console.log('Autoplay failed, user interaction required.');
-                setHasError(true)
+                setHasError(true); // Define o estado de erro
+                localStorage.setItem('hasError', 'true'); // Armazena o estado de erro
             }
         };
 
@@ -59,13 +61,13 @@ const BackgroundMusic = forwardRef((_, ref) => {
 
     useEffect(() => {
         if (audioRef.current) {
-            audioRef.current.muted = isMuted; // Aplica o estado de mutação ao áudio
+            audioRef.current.muted = isMuted; // Aplica o estado de mute ao áudio
         }
     }, [isMuted]);
 
     const toggleMute = () => {
         setIsMuted(prevMuted => {
-            const newMutedState = !prevMuted; // Inverte o estado de mutação
+            const newMutedState = !prevMuted; // Inverte o estado de mute
             localStorage.setItem('isMuted', newMutedState.toString()); // Armazena o novo estado
             return newMutedState; // Retorna o novo estado
         });
